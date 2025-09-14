77a34cd4cfb6ffeb8aef89af0dd946d75e420402
ShubhamKrishna0 <krishnashubham09@gmail.com>
2025-09-14 20:52:41 +0530

    heap lo

--- TUF/Heaps/Heapify Algorithm.cpp
+++ TUF/Heaps/Heapify Algorithm.cpp
@@ -3,48 +3,84 @@ using namespace std ;
 
 class Solution {
     private:
-    void heapifyDown(vector<int> &arr , int ind) {
-        int n = arr.size();
-        int smallest_Ind = ind;
+        vector<int> arr; //list to store the min heap
+        int count; //to store the current of the elements in the minheap
 
-        int leftChild_Ind = 2 * ind + 1, rightChild_Ind = 2 * ind + 2;
+        void heapifyUp(vector <int> &arr , int ind) {
+            int parentsInd = (ind - 1) / 2;
 
-        if(leftChild_Ind < n && arr[leftChild_Ind] < arr[smallest_Ind])
-            smallest_Ind = leftChild_Ind;
+            if(ind>0 && arr[ind] <arr[parentsInd]) {
+               swap(arr[ind], arr[parentsInd]);
 
-        if(rightChild_Ind < n && arr[rightChild_Ind] < arr[smallest_Ind])
-            smallest_Ind = rightChild_Ind;
-
-        if(smallest_Ind != ind) {
-            swap(arr[smallest_Ind], arr[ind]);
-
-            heapifyDown(arr, smallest_Ind);
+               heapifyUp(arr, parentsInd);
+            }
+            return;
         }
-        return;
-    }
-    //function to recursively heapify the array upwards
-    void heapifyUp(vector<int> &arr , int ind) {
-        int parent_Ind = (ind - 1) / 2;
 
-        //if current index holds 
-        if(ind>0 && arr[ind] < arr[parent_Ind]){
-            swap(arr[ind], arr[parent_Ind]);
-            heapifyUp(arr, parent_Ind);
+        void heapifyDown(vector<int> &arr , int ind) {
+            int n = arr.size();
+
+            int smallestInd = ind;
+            int leftChildInd = 2 * ind + 1, rightChildInd = 2 * ind + 2;
+
+            if(leftChildInd < n &&  arr[leftChildInd] <arr[smallestInd])
+                smallestInd = leftChildInd;
+            
+            if(rightChildInd < n && arr[rightChildInd] < arr[smallestInd])
+                smallestInd = rightChildInd;
+
+
+                if(smallestInd != ind) {
+                    swap(arr[smallestInd], arr[ind]);
+
+                    heapifyDown(arr, smallestInd);
+                }
+
+                return;
         }
-        return;
-    }
 
-    public:
-    void heapify (vector<int> &nums , int ind , int val) {
-        if(nums[ind] > val) {
-            nums[ind] = val;
-            heapifyUp(nums, ind);
-
-        }else{
-            nums[ind] = val;
-            heapifyDown(nums, ind);
-
+        public:
+        void initializeHeap() {
+            arr.clear();
+            count = 0;
         }
-        return;
-    }
+
+        void insert(int key) {
+            arr.push_back(key);
+            heapifyUp(arr, count);
+            count = count + 1;
+            return;
+        }
+        
+        void changeKey(int index , int new_val) {
+            if(arr[index] > new_val) {
+            arr[index] = new_val;
+            heapifyUp(arr, index);
+            }else{
+                arr[index] = new_val;
+                heapifyDown(arr, index);
+            }
+            return;
+        }
+// method to extract the minimum value from the min-heap
+        void extractMin() {
+            int ele = arr[0] ;
+            swap(arr[0], arr[count - 1]);
+
+            arr.pop_back();
+            count = count - 1;
+
+            heapifyDown(arr, 0);
+        }
+
+        bool isEmpty() {
+            return (count == 0);
+        }
+
+        int getMin() {
+            return arr[0];
+        }
+        int heapSize() {
+            return count;
+        }
 };
